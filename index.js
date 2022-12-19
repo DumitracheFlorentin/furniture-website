82ffda8932a900e6d5f04510e63a618f040f1e47
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2022-12-19 08:49:16 +0200

    done with static gallery

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const fs = require("fs")
+const sharp = require('sharp');
+
 app = express()
 
 app.set("view engine","ejs")
@@ -7,8 +9,61 @@ app.set("view engine","ejs")
 app.use("/resurse", express.static(__dirname + "/resurse"))
 
 var getErrors = null
+var getImages = null
 
-function createErrors() {
+function createImages () {
+  const file = fs.readFileSync(__dirname + "/resurse/data/galerie.json").toString("utf8")
+  const parsedObj = JSON.parse(file)
+  const medium_sizes = 200
+  const phone_sizes = 100
+  const nrOfImgs = 13
+  const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  const d = new Date();
+  const name = month[d.getMonth()];
+
+  getImages = parsedObj.images  
+
+  getImages.forEach(elem => {
+      [img_name, extension] = elem.path_name.split(".")
+      if(!fs.existsSync(parsedObj.path + "/medium/")){
+        fs.mkdirSync(parsedObj.path+"/medium/")
+      }
+
+      if(!fs.existsSync(parsedObj.path + "/phone/")){
+        fs.mkdirSync(parsedObj.path+"/phone/")
+      }
+
+      elem.path_name_medium = parsedObj.path + "/medium/" + img_name + ".webp"
+      elem.path_name_phone = parsedObj.path + "/phone/" + img_name + ".webp"
+      elem.path_name = parsedObj.path + "/" + elem.path_name
+
+      sharp(__dirname + "/" + elem.path_name).resize(medium_sizes).toFile(__dirname + "/" + elem.path_name_medium)
+      sharp(__dirname + "/" + elem.path_name).resize(phone_sizes).toFile(__dirname + "/" + elem.path_name_phone)
+    })
+
+    getImages = getImages.filter(img => {
+      if(["December", "January", "February"].includes(name)) {
+        return img.season == 'winter'
+      }
+      else if(["March", "April", "May"].includes(name)) {
+        return img.season == 'spring'
+      }
+      else if(["June","July","August"].includes(name)) {
+        return img.season == 'summer'
+      }
+      else {
+        return img.season == 'autumn'
+      }
+    })
+
+    while(getImages.length > nrOfImgs) {
+      getImages.pop()
+    }
+}
+
+createImages()
+
+function createErrors () {
   getErrors = JSON.parse(fs.readFileSync(__dirname + "/resurse/data/errors.json").toString("utf8"))
 }
 
@@ -29,7 +84,7 @@ function renderError (res, statusCode, title, label, img) {
 }
 
 app.get(["/","/index","/home"], (req, res) => {
-  res.render("pagini/index", { ip: req.ip })
+  res.render("pagini/index", { ip: req.ip, images: getImages })
 })
 
 app.get("/*.ejs",function(req, res){
